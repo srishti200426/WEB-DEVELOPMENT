18e5d8719a8fab5d082248915cbf49ae52a35b0c
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-02 23:57:19 +0530

    Added video 54

---
+++ video 54/script.js
@@ -0,0 +1,21 @@
+alert("hello world");
+console.log("code is running...")
+console.log("code is also running...")
+console.log("code is running like a wow...")
+
+var a = prompt("enter your number")
+var istrue = confirm("are you sure you want to leave this page and blast your computer ")
+
+if(istrue){
+    console.log("computer is blasting")
+}
+
+else{
+    console.log("computer is not blasting")
+}
+
+console.log("your number is" + a)
+
+document.title = " hey i am good"
+
+document.body.style.backgroundColor = "red"
