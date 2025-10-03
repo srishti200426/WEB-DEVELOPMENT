0cf9d387e9c8cb1009b22eac7db1eaa008530282
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-03 23:48:19 +0530

    Added video 55

---
+++ video 55/script.js
@@ -0,0 +1,43 @@
+console.log("hey this tutorial 55")
+
+let a =5;
+let b = 6;
+let c = "srishti";
+
+// var 55a = "rohan" not allowed
+
+// console.log(a + b + 8)
+// console.log(typeof a,typeof b, typeof c)
+{
+    let a = 66;
+    console.log(a)
+}
+
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1 + 6;
+// not allowed
+
+let x = "harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name" : "harry",
+    "job code":5600,
+    "is_handsome":true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "50crores";
+console.log(o);
+
