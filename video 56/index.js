0353a25d0f106ceac04c10f5ea5729271ca80e95
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-04 23:29:49 +0530

    Added video 56

---
+++ video 56/index.js
@@ -0,0 +1,14 @@
+console.log("hello i am conditional terminal")
+
+let age = 5;
+let grace = 2;
+console.log(age+grace);
+console.log(age**grace);
+console.log(age%grace);   
+console.log(age/grace);
+if((age+grace)>18){
+    console.log("you can drive");
+}
+else{
+    console.log("you cannot drive");
+}
