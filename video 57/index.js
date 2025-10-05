13671c73c11278f7cee07855e7cce88ff4ad51fa
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-05 23:33:21 +0530

    Added video 57

---
+++ video 57/index.js
@@ -0,0 +1,36 @@
+console.log("i am a tutorial on loops");
+let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+// let obj={
+//     name : "harry",
+//     role: "programmer",
+//     company: "codewithharry AI"
+// }
+
+// for (const key in obj) {
+    
+//         const element = obj[key];
+//         console.log(key,element);
+        
+    
+// }
+
+// for (const c of "harry") {
+//     console.log(c)
+    
+// }
+
+i = 10;
+// while(i<6){
+//     console.log(i);
+//     i++;
+// }
+
+do {
+    console.log(i);
+    i++;
+} while (i<6);
