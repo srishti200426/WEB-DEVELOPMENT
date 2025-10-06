3d8a22fb77dcc4728582b540bd9ad50625853251
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-06 23:33:33 +0530

    Added video 58

---
+++ video 58/index.js
@@ -0,0 +1,13 @@
+function nice(name) {
+    console.log("hey " + name + " you are nice ")
+}
+
+nice("rohan");
+nice("srishti")
+
+const func1 = (x)=>{
+    console.log("i am an arrow function ",x)
+}
+
+func1(34);
+func1(66);
