91b6941e6bb75ab9550e615f1d6a169b515f8b8b
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-08 23:46:11 +0530

    Added video 60

---
+++ video 60/index.js
@@ -0,0 +1,25 @@
+console.log("this is a tutorial on strings");
+let a = "harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a.length);
+
+let name = "harry";
+let friend = "rohan";
+console.log("name is: " + name + " and friends name is: " + friend);
+
+// template literals
+console.log(`his name is ${name} and his friends name is ${friend}`)
+
+let b = "shivamsh";
+let c = "SRISHTI"
+console.log(b.toUpperCase())
+console.log(c.toLowerCase())
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+
+console.log(b.replace("sh","77"))
+console.log(b.concat(a,"kapil"))
