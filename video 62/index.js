b9a04c99c8acbf25099a77f609418b0413368ce7
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-09 23:48:14 +0530

    Added video 62

---
+++ video 62/index.js
@@ -0,0 +1,41 @@
+mr = Math.random();
+
+if(0<mr<0.33){
+    console.log("crazy")
+}
+
+else if(0.33<mr<0.66){
+    console.log("amazing")
+}
+
+else{
+    console.log("fire")
+}
+
+mr1 = Math.random();
+
+if(0<mr1<0.33){
+    console.log("engine")
+}
+
+else if(0.33<mr1<0.66){
+    console.log("foods")
+}
+
+else{
+    console.log("garments")
+}
+
+mr2 = Math.random();
+
+if(0<mr2<0.33){
+    console.log("bro")
+}
+
+else if(0.33<mr2<0.66){
+    console.log("limited")
+}
+
+else{
+    console.log("hub")
+}
