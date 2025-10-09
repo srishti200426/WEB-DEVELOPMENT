04cd9f800375aaaa86ea61a14609371f998360bd
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-09 23:48:51 +0530

    Added video 63

---
+++ video 63/mfr.js
@@ -0,0 +1,32 @@
+let arr = [1,13,5,7,11];
+// let newarr= [];
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newarr.push(element**2)
+    
+// }
+
+// console.log(newarr)
+
+let newarr = arr.map((e)=>{
+    return e**2
+}
+)
+console.log(newarr)
+
+const greaterthanseven = (e) =>{
+    if(e>7){
+        return true
+    }
+    return false
+}
+
+console.log(newarr.filter(greaterthanseven))
+
+let arr2= [1,2,3,4,5,6]
+ const red =(a,b)=>{
+    return a+b
+ }
+
+console.log(arr2.reduce(red))
