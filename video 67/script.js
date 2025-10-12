ea6050622ccbec8cd924852646c8621eddca3fbf
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-12 23:55:58 +0530

    Added video 67

---
+++ video 67/script.js
@@ -0,0 +1,14 @@
+console.log("hello world")
+// document.body
+// <body>​<div class=​"container">​…​</div>​<script src=​"script.js">​</script>​</body>​
+// document.body.childNodes
+// NodeList(5) [text, div.container, text, script, text]
+// document.body.childNodes[0]
+// #text
+// document.body.childNodes[1]
+// // <div class=​"container">​…​</div>​
+// document.body.childNodes[1].childNodes
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children 
