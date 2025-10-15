14c4253889185b47e9be32cc85c19b90a2813aa2
srishti200426 <srishtimukhija.26@gmail.com>
2025-10-15 21:52:12 +0530

    Added video 71

---
+++ video 71/script1.js
@@ -0,0 +1,74 @@
+// document.querySelector(".box")
+// <div class=​"box">​hey i am a box​</div>​
+// document.querySelector(".box").innerHTML
+// 'hey i am a box'
+// document.querySelector(".box").innerText
+// // 'hey i am a box'
+// document.querySelector(".conatiner").innerHTML
+// VM692:1 Uncaught TypeError: Cannot read properties of null (reading 'innerHTML')
+//     at <anonymous>:1:37
+// (anonymous) @ VM692:1Understand this errorAI
+// document.querySelector(".container").innerHTML
+// '\n        <div class="box">hey i am a box</div>\n    '
+// document.querySelector(".container").outerHTML
+// '<div class="container">\n        <div class="box">hey i am a box</div>\n    </div>'
+// document.querySelector(".container").tagName
+// 'DIV'
+// document.querySelector(".container").nodeName
+// 'DIV'
+// document.querySelector(".container").textContent
+// '\n        hey i am a box\n    '
+// document.querySelector(".container").hidden
+// false
+// document.querySelector(".box").innerHTML = "hey i am haryr"
+// 'hey i am haryr'
+
+// document.querySelector(".box")
+// <div class=​"box">​hey i am a box​</div>​
+// document.querySelector(".box").innerHTML
+// 'hey i am a box'
+// document.querySelector(".box").innerText
+// 'hey i am a box'
+// document.querySelector(".conatiner").innerHTML
+// VM692:1 Uncaught TypeError: Cannot read properties of null (reading 'innerHTML')
+//     at <anonymous>:1:37
+// (anonymous) @ VM692:1Understand this errorAI
+// document.querySelector(".container").innerHTML
+// '\n        <div class="box">hey i am a box</div>\n    '
+// document.querySelector(".container").outerHTML
+// '<div class="container">\n        <div class="box">hey i am a box</div>\n    </div>'
+// document.querySelector(".container").tagName
+// 'DIV'
+// document.querySelector(".container").nodeName
+// 'DIV'
+// document.querySelector(".container").textContent
+// '\n        hey i am a box\n    '
+// document.querySelector(".container").hidden
+// false
+// document.querySelector(".box").innerHTML = "hey i am haryr"
+// 'hey i am haryr'
+
+// document.designMode = "on"
+// 'on'
+
+// document.querySelector(".box").dataset
+// DOMStringMap {createdby: 'harry'}
+
+// document.querySelector(".container").classList
+// DOMTokenList(3) ['container', 'red', 'bg-green', value: 'container red bg-green']
+
+// document.querySelector(".container").className
+// 'container red bg-green'
+// document.querySelector(".container").classList.add("harry")
+// undefined
+// document.querySelector(".container").classList.remove("harry")
+// undefined
+// document.querySelector(".container").classList.remove("red")
+// undefined
+// document.querySelector(".container").classList.add("red")
+// undefined
+
+// document.querySelector(".container").classList.toggle("red")
+// false
+// document.querySelector(".container").classList.toggle("red")
+// true
